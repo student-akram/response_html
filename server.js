fa6e42aee92eb141eff4995fc97c77b5dc8bf4cd
Akram Student <akramshaik2004@gmail.com>
2026-02-13 19:42:41 +0530

    POST request with HTML and Axios

--- server.js
+++ server.js
@@ -3,9 +3,22 @@ const path = require("path");
 
 const app = express();
 
-// GET endpoint
+// middleware to read JSON
+app.use(express.json());
+
+// Serve form
 app.get("/api/products", (req, res) => {
   res.sendFile(path.join(__dirname, "views", "form.html"));
+});
+
+// Handle POST
+app.post("/api/products", (req, res) => {
+
+  const product = req.body.productName;
+
+  console.log("Received:", product);
+
+  res.send(`Product Added: ${product}`);
 });
 
 app.listen(3000, () => {
